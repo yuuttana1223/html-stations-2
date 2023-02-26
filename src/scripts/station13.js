f1cd921a7c4678e68cbc0d11fe785c3bf2060aa0
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-02-26 16:46:06 +0900

    ✨ station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const checked = e.target.checked;
+  const text = document.getElementById("text");
+  if (checked) {
+    text.style.backgroundColor = "red";
+    return;
+  }
+  text.style.backgroundColor = "transparent";
 }
